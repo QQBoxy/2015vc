8f4f2d5ddba540983b634febd1cd1f32f5e5765b
QQBoxy <gs1458@gmail.com>
2015-07-16 08:25:23 +0800

    Add inherit

---
+++ inherit/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Point2D {
+public:
+    Point2D() {
+        this->x = 0;
+        this->y = 0;
+    }
+public:
+    int x;
+    int y;
+};
+
+class Point3D : public Point2D {
+public:
+    Point3D() { 
+        this->z = 0;
+    }
+public:
+    int z;
+};
+
+int main(int argc, char* argv[]) {
+
+    Point3D newpoint;
+    
+    newpoint.x = 1;
+    newpoint.y = 2;
+    newpoint.z = 3;
+    
+    cout << newpoint.x << ", " << newpoint.y << ", " << newpoint.z << endl;
+
+    system("pause");
+    return 0;
+}
