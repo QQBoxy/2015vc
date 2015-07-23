25f50c2e6c80f3b9997a260617e77e852776b9eb
QQBoxy <gs1458@gmail.com>
2015-07-23 08:36:04 +0800

    Edit Default Adapter

--- defaultAdapter/main.cpp
+++ defaultAdapter/main.cpp
@@ -3,10 +3,18 @@ using namespace std;
 
 class Deque {
 public:
-    void push_back(int x) { cout<<"Deque push_back"<<endl; }
-    void push_front(int x) { cout<<"Deque push_front"<<endl; }
-    void pop_back() { cout<<"Deque pop_back"<<endl; }
-    void pop_front() { cout<<"Deque pop_front"<<endl; }
+    void push_back(int x) {
+    	cout<<"Deque push_back"<<endl;
+    }
+    void push_front(int x) {
+    	cout<<"Deque push_front"<<endl;
+    }
+    void pop_back() {
+    	cout<<"Deque pop_back"<<endl;
+    }
+    void pop_front() {
+    	cout<<"Deque pop_front"<<endl;
+    }
 };
 
 class Sequence {
@@ -15,31 +23,43 @@ public:
     virtual void pop() = 0;
 };
 
-class Stack: public Sequence {
+class Stack : public Sequence {
 public:
-    void push(int x) { deque.push_back(x); }
-    void pop() { deque.pop_back(); }
+    void push(int x) {
+    	deque.push_back(x);
+    }
+    void pop() {
+    	deque.pop_back();
+    }
 private:
     Deque deque;
 };
 
-class Queue: public Sequence {
+class Queue : public Sequence {
 public:
-    void push(int x) { deque.push_back(x); }
-    void pop() { deque.pop_front(); }
+    void push(int x) {
+    	deque.push_back(x);
+    }
+    void pop() {
+    	deque.pop_front();
+    }
 private:
     Deque deque;
 };
-
 
 int main(void) {
-
     Sequence *s1 = new Stack();
     Sequence *s2 = new Queue();
-    s1->push(1); s1->pop();
-    s2->push(1); s2->pop();
-    delete s1; delete s2;
-
+    
+    s1->push(1);
+    s1->pop();
+    
+    s2->push(1);
+    s2->pop();
+    
+    delete s1;
+    delete s2;
+    
     system("pause");
     return 0;
 }
